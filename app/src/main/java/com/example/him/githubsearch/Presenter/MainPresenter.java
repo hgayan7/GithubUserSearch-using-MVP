5c2f3db88230e5724fb904763ee15c2cf27d6d03
Himshikhar Gayan <hgayan7@gmail.com>
2018-02-11 19:07:13 +0530

    RxJava added

--- app/src/main/java/com/example/him/githubsearch/Presenter/MainPresenter.java
+++ app/src/main/java/com/example/him/githubsearch/Presenter/MainPresenter.java
@@ -3,14 +3,12 @@ package com.example.him.githubsearch.Presenter;
 
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 import com.example.him.githubsearch.Interface.MainContract;
 import com.example.him.githubsearch.Model.RestResponse;
 import com.example.him.githubsearch.Rest.ApiClient;
 import com.example.him.githubsearch.Rest.ApiService;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import static android.content.ContentValues.TAG;
 
 /**
@@ -31,27 +29,25 @@ public class MainPresenter implements MainContract.mvpPresenter{
     @Override
     public void loadData(final View view, String userId) {
 
-        Call<RestResponse> call=apiService.getData(userId);
-        call.enqueue(new Callback<RestResponse>() {
-            @Override
-            public void onResponse(Call<RestResponse> call, Response<RestResponse> response) {
+        apiService.getData(userId)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResult,this::handleError);
 
-                RestResponse restResponse = response.body();
-                if (response.isSuccessful()) {
-                    mvpView.showData(restResponse.getAvatarUrl(), restResponse.getName(),
-                            String.valueOf(restResponse.getFollowers()), String.valueOf(restResponse.getPublicRepos()));
-                }else {
-                    Toast.makeText(view.getContext(), "Enter a valid username", Toast.LENGTH_SHORT).show();
-                }
-            }
+    }
 
-            @Override
-            public void onFailure(Call<RestResponse> call, Throwable t) {
+    private void handleResult(RestResponse restResponse){
 
-                Log.d(TAG, "onFailure: "+t);
+        if(restResponse!=null){
+            mvpView.showData(restResponse.getAvatarUrl(), restResponse.getName(),
+                    String.valueOf(restResponse.getFollowers()), String.valueOf(restResponse.getPublicRepos()));
+        }else {
+            Log.d(TAG, "handleResult: "+"Enter a valid username");
+        }
+    }
 
-            }
-        });
+    private void handleError(Throwable throwable){
 
+        Log.d(TAG, "handleError: "+throwable);
     }
 }
